d6408e590c47e0af9e4e55144f67aadb93ada617
Freddy Mallet <freddy.mallet@gmail.com>
2017-04-04 23:50:40 +0200

    Add Quizz Dead Store

--- src/main/java/devoxx/QuizzDeadStore.java
+++ src/main/java/devoxx/QuizzDeadStore.java
@@ -1,50 +1,43 @@
 package devoxx;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class QuizzDeadStore {
 
-  public int totalAnimalWeight(Path csvPath) {
-    int result = 0;
-    try (BufferedReader reader = new BufferedReader(new FileReader(csvPath.toFile()))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        if (shouldSkip(line)) {
-          continue;
-        }
-        result += parseWeight(line);
+  public void playWith(int number, int playerAge) {
+    String message;
+    int amountOfMoney;
+    switch (number) {
+    case 0:
+      amountOfMoney = 1_000;
+      message = "A bit defensive but deserves a reward!";
+      break;
+    case -1:
+      amountOfMoney = 0;
+      message = "Are you trying to hack me ?";
+      break;
+    case 42:
+      switch(playerAge) {
+      case 2 :
+        amountOfMoney = 1;
+        message = "Are you kidding me?";
+        break;      
+      case 42 :
+        amountOfMoney = 10_000;
+        message = "Well done + extra bonus due to your age!";
+        break;   
+      default:
+        amountOfMoney = 5_000;
+        message = "Well done!";
+        break;
       }
-
-    } catch (IOException e) {
-      //LOG.error("Unable to open file {}", csvPath, e);
-      return 0;
+    default:
+      throw new RuntimeException("Game Over!");
     }
-    return result;
+    give(amountOfMoney, message);
   }
 
-  private boolean shouldSkip(String line) {
-    String trimmedLine = trimToNull(line);
-    return !trimmedLine.startsWith("#");
+  private void give(int amountOfMoney, String message) {
+    Logger.getGlobal().info(message + ", " + amountOfMoney + " for you");
   }
-
-  private static String trimToNull(String line) {
-    String trimmed = line.trim();
-    return trimmed.length() > 0 ? trimmed : null;
-  }
-
-  private int parseWeight(String line) {
-    String[] cells = line.split(",");
-    if (cells.length >= 3 && isNotBlank(cells[2])) {
-      return Integer.parseInt(cells[2]);
-    }
-    return 0;
-  }
-
-  private static boolean isNotBlank(String str) {
-    return str != null && str.trim().length() > 0;
-  }
-
 }
