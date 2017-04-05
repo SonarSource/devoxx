6562dfbbce4972fef3e2503f2a0231487b98307d
Freddy Mallet <freddy.mallet@gmail.com>
2017-04-05 09:29:03 +0200

    Simplify Unreachable Condition Example

--- src/main/java/devoxx/QuizzUnreachableConditionalBranch.java
+++ src/main/java/devoxx/QuizzUnreachableConditionalBranch.java
@@ -2,10 +2,8 @@ package devoxx;
 
 public class QuizzUnreachableConditionalBranch {
 
-  public int ultimateBoringBusinessLogic(int antonio, int nicolas, int zouheir,
-      int arnaud) {
-    if (antonio > nicolas && nicolas == zouheir && zouheir > arnaud
-        && arnaud > antonio) {
+  public int ultimateBoringBusinessLogic(int antonio, int nicolas, int zouheir) {
+    if (antonio > nicolas && nicolas == zouheir && zouheir > antonio) {
       return Integer.MAX_VALUE;
     }
     return -1;
