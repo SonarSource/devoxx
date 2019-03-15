8184b879d68ca7755466bc7dd65d66cc038028ea
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2019-03-15 15:28:30 +0100

    Adding issues for PR decoration demo

---
+++ src/main/java/devoxx/Simplest.java
@@ -0,0 +1,12 @@
+public class Simplest {
+public String name;
+
+public static int add(int a, int b) {
+        int result = a + b;
+
+        System.out.println("Result = " + result);
+
+        return result;
+        }
+}
+        
